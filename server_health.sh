57344e92f234cdb8e8529ef3089fadfeff621d54
naiduklr936 <naiduklr936@gmail.com>
2017-07-29 13:27:47 -0500

    Update server_health.sh

--- server_health.sh
+++ server_health.sh
@@ -1,3 +1,3 @@
 #! bin/bash
 
-sudo wget  --connect-timeout='time in seconds' --tries='total number of tries' http://HostIP 2> /dev/null || echo "Website down or Not Connecting." | mail -s "Website is down" username@domain.com
+wget  --connect-timeout='time in seconds' --tries='total number of tries' http://HostIP 2> /dev/null || echo "Website down or Not Connecting." | mail -s "Website is down" username@domain.com
